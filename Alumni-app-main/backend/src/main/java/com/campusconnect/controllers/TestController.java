1d541e87978e9f22f0328fbf97aef82e7134bbc2
shubham kadam <mayurkadam19237@gmail.com>
2026-04-12 19:53:36 +0530

    last commit

--- Alumni-app-main/backend/src/main/java/com/campusconnect/controllers/TestController.java
+++ Alumni-app-main/backend/src/main/java/com/campusconnect/controllers/TestController.java
@@ -4,7 +4,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class testcontroller {
+public class TestController {
 
     @GetMapping("/")
     public String home() {
